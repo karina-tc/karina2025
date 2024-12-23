6d369e9acd3772d0d46f8737de74fb809fa18bb3
karina <hllo@karinatovar.com>
2024-12-23 15:20:29 -0500

    changes to copy colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,12 +9,15 @@ export default {
       },
       colors: {
         'background-color': 'var(--background-color)',
-        'text-color': 'var(--text-color)',
+        'copy-primary': 'var(--copy-primary)',
+        'copy-secondary': 'var(--copy-secondary)',
       },
       animation: {
         'fade-in': 'fade-in 0.5s ease-in-out',
         'fade-in-up': 'fade-in-up 0.5s ease-in-out',
         'fade-in-down': 'fade-in-down 0.5s ease-in-out',
+        'fade-in-left': 'fade-in-left 0.5s ease-in-out',
+        'fade-in-right': 'fade-in-right 0.5s ease-in-out',
       },
       keyframes: {
         'fade-in': {
@@ -28,6 +31,14 @@ export default {
         'fade-in-down': {
           '0%': { opacity: '0', transform: 'translateY(-100%)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        'fade-in-left': {
+          '0%': { opacity: '0', transform: 'translateX(-100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        'fade-in-right': {
+          '0%': { opacity: '0', transform: 'translateX(100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
         },
       },
     },
