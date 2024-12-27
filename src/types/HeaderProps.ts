4673ab2ca8a6024f0a6b22984ccce0604ee076c1
karina <hllo@karinatovar.com>
2024-12-27 12:02:29 -0500

    image optimization

--- src/types/HeaderProps.ts
+++ src/types/HeaderProps.ts
@@ -1,3 +1,4 @@
+import type { ImageMetadata } from 'astro';
 export interface TitleProps {
   logo?: string;
   logo_alt_text?: string;
@@ -10,17 +11,17 @@ export interface TitleProps {
 
 export interface HorizonProps {
   horizon_height?: string;
-  visual_image?: string;
+  visual_image?: ImageMetadata;
   visual_alt_text?: string;
   visual_classes?: string;
-  visual_width?: string;
+  visual_width?: number;
 }
 
 export interface HeaderProps extends TitleProps {
   horizon_height?: string;
   title_classes?: string;
-  header_visual_image?: string;
+  header_visual_image?: ImageMetadata;
   header_visual_alt_text?: string;
   header_visual_classes?: string;
-  header_visual_width?: string;
+  header_visual_width?: number;
 } 
