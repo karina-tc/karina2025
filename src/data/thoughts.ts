f1765840f2494e0f15ffe48fa0ba580df2def493
karina <hllo@karinatovar.com>
2024-12-27 17:10:32 -0500

    dummy content for blog

---
+++ src/data/thoughts.ts
@@ -0,0 +1,44 @@
+export const thoughts = [
+  {
+    title: "Products as Shells",
+    category: "Strategy",
+    readingTime: 4,
+    date: "2024-03-15",
+    slug: "products-as-shells"
+  },
+  {
+    title: "Your Figma Design System Lies",
+    category: "Design",
+    readingTime: 6,
+    date: "2024-02-28",
+    slug: "your-figma-design-system-lies"
+  },
+  {
+    title: "The Botany of Components",
+    category: "Development",
+    readingTime: 3,
+    date: "2024-01-15",
+    slug: "the-botany-of-components"
+  },
+ {
+    title: "When Becoming an Expert is a Bad Idea",
+    category: "Writing",
+    readingTime: 5,
+    date: "2024-01-10",
+    slug: "when-becoming-an-expert-is-a-bad-idea"
+  },
+  {
+    title: "Saying No to Asses",
+    category: "Strategy",
+    readingTime: 4,
+    date: "2025-01-02",
+    slug: "saying-no-to-asses"
+  },
+  {
+    title: "The Rise of the Unicorn",
+    category: "Development",
+    readingTime: 4,
+    date: "2025-01-02",
+    slug: "the-rise-of-the-unicorn"
+  }
+]; 
