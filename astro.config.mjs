b942918a756a5a06f3c6f205231146436b67742e
karina <hllo@karinatovar.com>
2024-12-30 13:21:56 -0500

    adds dummy posts markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import prefetch from '@astrojs/prefetch';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch()],
+  integrations: [tailwind(), prefetch(), mdx()],
   adapter: netlify(),
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'vesper',
+      theme: 'night-owl',
       wrap: true,
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
