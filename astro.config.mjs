860bfdb668396f3529dd667222a5260a005239b3
karina <hllo@karinatovar.com>
2024-12-30 15:21:18 -0500

    theme change

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'night-owl',
+      theme: 'one-light', // night-owl
       wrap: true,
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
