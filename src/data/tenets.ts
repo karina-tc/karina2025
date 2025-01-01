098dff865c3b1ecd41724816a679a16ee4967711
karina <hllo@karinatovar.com>
2025-01-01 12:02:02 -0500

    changs to tenets

---
+++ src/data/tenets.ts
@@ -0,0 +1,31 @@
+import image1 from "../assets/other/plant.png";
+import image2 from "../assets/other/clipboard.png";
+import image3 from "../assets/other/cup.png";
+import image4 from "../assets/other/notes.png";
+
+export const tenets = [
+  {
+    number: "01",
+    title: "Team Ideation",
+    description: "Ideas can come from anywhere. Don't let ego get in the way of your team's success.",
+    image: image1,
+  },
+  {
+    number: "02",
+    title: "Diligent Speed",
+    description: "Work as fast as you can to understand the problem and deliver a worthwhile economic solution.",
+    image: image2
+  },
+  {
+    number: "03",
+    title: "Inclusivity",
+    description: "Design should be accessible to everyone. It's the right thing and it brings true usability to your solution.",
+    image: image3
+  },
+  {
+    number: "04",
+    title: "Keep Playing",
+    description: "Go further than building delight. Think of the long term and how your solution can grow.",
+    image: image4
+  }
+]; 
